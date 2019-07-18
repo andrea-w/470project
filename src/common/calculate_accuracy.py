2cda803f20c81263982ab8293c019cd79854d971
Andrea <andrea.k.williams7@gmail.com>
2019-07-18 13:34:21 -0700

    Partially completed script to calculate accuracy of candidate

--- src/common/calculate_accuracy.py
+++ src/common/calculate_accuracy.py
@@ -2,3 +2,16 @@ import pandas
 
 def calculate_accuracy():
     return
+
+# returns the Mean Squared Error for an arraylist of predicted test scores
+# against the arraylist of the actual test scores
+def calculate_MSE(actual_scores, predicted_scores):
+    # first perform error checking
+    if (actual_scores.len() != predicted_scores.len()):
+        return "Error. Mismatch in length of arraylists of scores"
+    N = actual_scores.len()
+    squared_error = 0
+    for i in range(N):
+        squared_error += (actual_scores[i] - predicted_scores[i])**2
+    # return average squared error
+    return squared_error/N
